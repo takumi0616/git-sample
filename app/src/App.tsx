2062c77b6e21e03508ba9a546da0c809a6b13037
takumi0616 <s233319@stn.nagaokaut.ac.jp>
2023-06-21 10:20:31 +0900

    プログラム追加

---
+++ app/src/App.tsx
@@ -0,0 +1,180 @@
+import React ,{ Fragment} from 'react';
+
+//変数の宣言
+const message: string = 'こんにちは React!!'
+const num: number = 3;
+const bool:boolean = true;
+const numArr: number[] = [10,20,30];
+const strArr: string[] = ["りんご","メロン"];
+
+//変数同士の計算
+const num1: number = num + numArr[1];
+const message1: string = message + strArr[1];
+
+//変数にHTMLのコードを入れられる
+const element = <h1 title="hello">Hello JSX!</h1>;
+
+//アロー関数
+const returnStrings = () => {
+  return 'アロー関数で返された文字列です'
+};
+const returnSt = <h2>{returnStrings()}</h2>;
+
+//function
+function returnStrings1(){
+  return 'functionで返された文字列です'
+}
+const returnSt1 = <h2>{returnStrings1()}</h2>;
+
+//繰り返し文
+const  forproducts = () => {
+    const numArr1 = [];
+    for (let i = 0; i < 5; i++) {
+      numArr1.push(<p>{i}</p>);
+    }
+    return numArr1;
+};
+
+//if文-書き方1　三項演算子
+const ifproducts = () => {
+  return (
+    <Fragment>
+      <div>{ bool ? 'hello world0' : 'Not hello world' }</div>
+    </Fragment>
+  );
+};
+//if文-書き方2　&&を使う
+const ifproducts1 = () => {
+  const isFlag = true;
+  return (
+    <Fragment>
+      <div>{ isFlag && bool && "hello world1" }</div>
+    </Fragment>
+  );
+};
+
+//オブジェクト型定義1
+type Person = {
+  firstName: string,
+  lastName: string,
+  age:number | undefined,
+  isStudent: boolean
+};
+//
+const person1 : Person = {
+  firstName :'takasuka',
+  lastName : 'takumi',
+  age: undefined,
+  isStudent : true
+};
+//オブジェクトの型に変数を使うこともできる
+const age1 = 'age';
+//オブジェクトの型定義2
+let person2: {
+  name:string;
+  [age1]: number;
+} = {
+  name: 'alice',
+  age: 25,
+};
+
+//Buttonの表示、alertでHelloが出る
+const Button1 = () => {
+  function greeting() {
+    alert("Hello!");
+  }
+
+return (
+    <div>
+      <button onClick={greeting}>Click me!</button>
+    </div>
+  );
+};
+
+
+
+
+
+// export const TestUseState = () => {
+//   const [email, setEmail] = useState()
+
+//   useEffect(() => {
+//     setEmail('test@example.com')
+//   }, [])
+
+//   return (
+//     <div className="m-3 col-4">
+//       <div>useStateのテスト</div>
+//       <div className="card">
+//         <div className="card-body">
+//           <div>メールアドレス：{email}</div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+//export default TestUseState
+
+
+//Hello,world表示,変数の表示
+function App() {
+  return (
+    <div>
+      {element}
+    {returnSt}
+    {returnSt1}
+    <h1>Hello,world!</h1>
+    <p>{message}</p>
+    <p>{num}</p>
+    <p>{bool}</p>
+    <p>{numArr[0]}</p>
+    <p>{numArr[1]}</p>
+    <p>{numArr[2]}</p>
+    <p>{strArr[0]}</p>
+    <p>{strArr[1]}</p>
+    <p>{num1}</p>
+    <p>{message1}</p>
+    <p>It`s {num}</p>
+    <p>{num + numArr[1]}</p>
+    {forproducts()}
+    {ifproducts()}
+    {ifproducts1()}
+    <p>{person1.firstName}</p>
+    <p>{person2.name}</p>
+    {Button1()}
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+//import logo from './logo.svg';
+//import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           編集 <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ app/src/index.tsx
@@ -0,0 +1,28 @@
+//import React from 'react';
+import React from "react";
+import ReactDom from 'react-dom';
+//import ReactDOM from 'react-dom/client';
+//, Component ,useEffect ,useState
+//import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+//import { isConstructorDeclaration } from "typescript";
+//import { render } from "@testing-library/react";
+
+ReactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+
